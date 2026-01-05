901079c8353cbb93ea03e9627bd1a159bd70b4f3
Srikanth <sreekanth.rayapalli@gmail.com>
2026-01-05 15:48:06 +0530

    commit

---
+++ dept-service/src/main/java/com/tcs/springcoreexample/basic/DeptServiceApplication.java
@@ -0,0 +1,19 @@
+package com.tcs.springcoreexample.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DeptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
